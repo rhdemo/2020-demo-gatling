b8fd46d8c60d94a696690194e1aac7a4970851d2
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2020-04-07 13:11:36 -0300

    Including Load Test Scripts and Report Grabber

--- Makefile
+++ Makefile
@@ -2,8 +2,7 @@ ENV_FILE := .env
 include ${ENV_FILE}
 export $(shell sed 's/=.*//' ${ENV_FILE})
 
-
-oc_login:
+oc_login: 
 	oc login ${OC_URL} -u ${OC_USER} -p ${OC_PASSWORD} --insecure-skip-tls-verify=true
 
 build-image:
@@ -31,3 +30,7 @@ deploy-load-test:
 remove-load-test:
 	oc project ${NAMESPACE}
 	oc process -f openshift/template.yaml -p $ USERS=${USERS} NAMESPACE=${NAMESPACE} GUESSES=${GUESSES} PERCENT_BAD_GUESSES=${PERCENT_BAD_GUESSES} IMAGE=${IMAGE} REPLICAS=${REPLICAS} SOCKET_ADDRESS=${SOCKET_ADDRESS} SIMULATION=${SIMULATION} | oc delete -f -
+
+report:
+	oc project ${NAMESPACE}
+	oc get pods -o name | xargs -n 1 oc logs --tail=50 > logs/${NAMESPACE}.log
--- scripts/grab-test-report.sh
+++ scripts/grab-test-report.sh
@@ -1,39 +1,25 @@
-
-export SIMULATION=E2ESimulation
-
-# Load Tester Params
-export REPLICAS=1
-export USERS=2000
-export GUESSES=300
-export PERCENT_BAD_GUESSES=100
-
-
 export NAMESPACE=load-test-sf1
 export OC_URL=https://api.summit-aws-sf1.openshift.redhatkeynote.com:6443
-export SOCKET_ADDRESS=game-frontend.apps.summit-aws-sf1.openshift.redhatkeynote.com/socket
 make oc_login
-make create-namespace
-make deploy-load-test 
+make report
 
 
 export NAMESPACE=load-test-lnd1
 export OC_URL=https://api.summit-aws-lnd1.openshift.redhatkeynote.com:6443
 export SOCKET_ADDRESS=game-frontend.apps.summit-aws-lnd1.openshift.redhatkeynote.com/socket
 make oc_login
-make create-namespace
-make deploy-load-test 
+make report
 
 
 export NAMESPACE=load-test-ny
 export OC_URL=https://api.summit-gcp-ny1.redhatgcpkeynote.com:6443
 export SOCKET_ADDRESS=game-frontend.apps.summit-gcp-ny1.redhatgcpkeynote.com/socket
 make oc_login
-make create-namespace
-make deploy-load-test 
+make report
+
 
 export NAMESPACE=load-test-ffm1
 export OC_URL=https://api.summit-gcp-ffm1.redhatgcpkeynote.com:6443
 export SOCKET_ADDRESS=game-frontend.apps.summit-gcp-ffm1.redhatgcpkeynote.com/socket 
 make oc_login
-make create-namespace
-make deploy-load-test 
+make report
