ff45ab4eb96e11e9519a54676f12be95eb8c6de0
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2020-04-07 13:11:36 -0300

    Including Load Test Scripts

---
+++ scripts/remove-load.sh
@@ -0,0 +1,26 @@
+export NAMESPACE=load-test-sf1
+export OC_URL=https://api.summit-aws-sf1.openshift.redhatkeynote.com:6443
+export SOCKET_ADDRESS=game-frontend.apps.summit-aws-sf1.openshift.redhatkeynote.com/socket
+make oc_login
+make delete-namespace
+
+
+
+export NAMESPACE=load-test-lnd1
+export OC_URL=https://api.summit-aws-lnd1.openshift.redhatkeynote.com:6443
+export SOCKET_ADDRESS=game-frontend.apps.summit-aws-lnd1.openshift.redhatkeynote.com/socket
+make oc_login
+make delete-namespace
+
+
+export NAMESPACE=load-test-ny
+export OC_URL=https://api.summit-gcp-ny1.redhatgcpkeynote.com:6443
+export SOCKET_ADDRESS=game-frontend.apps.summit-gcp-ny1.redhatgcpkeynote.com/socket
+make oc_login
+make delete-namespace
+
+export NAMESPACE=load-test-ffm1
+export OC_URL=https://api.summit-gcp-ffm1.redhatgcpkeynote.com:6443
+export SOCKET_ADDRESS=game-frontend.apps.summit-gcp-ffm1.redhatgcpkeynote.com/socket 
+make oc_login
+make delete-namespace
